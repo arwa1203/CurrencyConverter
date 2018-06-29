2ee786d07c17f61e6d38f4f9876ab1589470ee7a
Arwa <arwa.ahmed1203@gmail.com>
2018-06-29 15:45:40 -0500

    inital commit

---
+++ app/src/main/java/com/example/arwa_ahmed/currencyconverter/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.arwa_ahmed.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final EditText amount1 = (EditText) findViewById(R.id.amount1);
+        amount1.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    Double firstAmountDb = Double.parseDouble(amount1.getText().toString());
+
+                    EditText amount2 = (EditText) findViewById(R.id.amount2);
+                    Double secondAmountDb= firstAmountDb * 0.75;
+
+                    amount2.setText(secondAmountDb.toString());
+
+
+                    handled = true;
+                    setContentView(R.layout.activty_main);
+                }
+                return handled;
+            }
+        });
+
+
+
+    }
+}
